1840234206933ea011dd3ab73ed9bf03a7408852
Sauda11 <raeesa_kakti@yahoo.co.in>
2026-05-08 19:24:27 +0530

    f

--- act2.py
+++ act2.py
@@ -2,29 +2,32 @@ import turtle
 
 turtle.Screen().bgcolor("Aqua")
 board = turtle.Turtle()
- 
+board.fillcolor("blue")
+board.begin_fill()
 # first triangle for star
 board.forward(100) # draw base
- 
 board.left(120)
+
 board.forward(100)
- 
 board.left(120)
+
 board.forward(100)
- 
+#second triangle 
 board.penup()
 board.right(150)
-board.forward(50)
+board.forward(50) 
  
 # second triangle for star
 board.pendown()
 board.right(90)
+
 board.forward(100)
- 
 board.right(120)
+
 board.forward(100)
- 
 board.right(120)
+
 board.forward(100)
- 
+
+board.end_fill()
 turtle.done()
